5ef0124488c8cc1a71aae031d9267d8144287062
Kaden Wu <kadenlinwu@gmail.com>
2025-09-03 16:41:00 -0700

    remove api bloat

---
+++ vlrggapi/scrapers/__init__.py
@@ -0,0 +1,6 @@
+from .rankings import vlr_rankings
+from .stats import vlr_stats
+from .matches import vlr_upcoming_matches, vlr_live_score, vlr_match_results
+from .events import vlr_events
+from .health import check_health
+from .news import vlr_news
