01d6758ee2bd3ea7ba927f19438fe5dd3865560a
Kaden Wu <kadenlinwu@gmail.com>
2025-09-03 15:05:39 -0700

    make frontend/backend template

---
+++ backend/src/main/java/com/valorant/ValoPredict/ValoPredictApplication.java
@@ -0,0 +1,13 @@
+package com.valorant.ValoPredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValoPredictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValoPredictApplication.class, args);
+	}
+
+}
