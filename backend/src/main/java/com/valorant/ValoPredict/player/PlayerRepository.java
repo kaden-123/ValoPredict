6c05340189d584932f25207cbd94b5ddd2bcea22
David An <totallydavidahn@gmail.com>
2025-09-06 14:35:15 -0700

    PlayerRepository interface created

--- backend/src/main/java/com/valorant/ValoPredict/player/PlayerRepository.java
+++ backend/src/main/java/com/valorant/ValoPredict/player/PlayerRepository.java
@@ -1,4 +1,10 @@
 package com.valorant.ValoPredict.player;
 
-public interface PlayerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PlayerRepository extends JpaRepository<Player, Integer> {
+    // void deleteById(Integer id);
+    // Optional<Player> findById(Integer id);
 }
