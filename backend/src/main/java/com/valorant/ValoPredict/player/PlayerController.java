5b2d179448b301f54216794a53a399e425560923
David An <totallydavidahn@gmail.com>
2025-09-06 16:59:06 -0700

    create player controller with basic request/response functionality

---
+++ backend/src/main/java/com/valorant/ValoPredict/player/PlayerController.java
@@ -0,0 +1,71 @@
+package com.valorant.ValoPredict.player;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+
+    private final PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping
+    public List<Player> getPlayers(
+            @RequestParam(required=false) String org,
+            @RequestParam(required=false) String name,
+            @RequestParam(required=false) String agent){
+        if(org != null && agent != null){
+            return playerService.getPlayersByOrgAndAgent(org, agent);
+        }
+
+        else if(org != null){
+            return playerService.getPlayersFromOrg(org);
+        }
+
+        else if(agent != null){
+            return playerService.getPlayersByAgent(agent);
+        }
+
+        else if(name != null){
+            return playerService.getPlayersByName(name);
+        }
+
+        else{
+            return playerService.getPlayers();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Player> addPlayer(@RequestBody Player player){
+        Player newPlayer = playerService.addPlayer(player);
+        return new ResponseEntity<>(newPlayer, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Player> updatePlayer(@RequestBody Player player){
+        Player updatedPlayer = playerService.updatePlayer(player);
+
+        if(updatedPlayer != null){
+            return new ResponseEntity<>(updatedPlayer, HttpStatus.OK);
+        }
+
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{playerId}")
+    public ResponseEntity<String> deletePlayer(@PathVariable Integer playerId){
+        playerService.deletePlayer(playerId);
+        return new ResponseEntity<>("Player deleted successfully", HttpStatus.OK);
+    }
+}
