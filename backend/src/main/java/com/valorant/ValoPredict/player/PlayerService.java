20595313d57897e5aa982626973d7c0522dd88a0
David An <totallydavidahn@gmail.com>
2025-09-08 12:26:56 -0700

    Split agents by comma delimiter

--- backend/src/main/java/com/valorant/ValoPredict/player/PlayerService.java
+++ backend/src/main/java/com/valorant/ValoPredict/player/PlayerService.java
@@ -37,14 +37,14 @@ public class PlayerService {
 
     public List<Player> getPlayersByAgent(String searchText){
         return playerRepository.findAll().stream()
-                .filter(player -> Arrays.stream(player.getAgents())
+                .filter(player -> Arrays.stream(player.getAgents().split(", "))
                         .anyMatch(agent -> agent.equalsIgnoreCase(searchText)))
                 .collect(Collectors.toList());
     }
 
     public List<Player> getPlayersByOrgAndAgent(String org, String agentName){
         return playerRepository.findAll().stream()
-                .filter(player -> player.getOrg().equalsIgnoreCase(org) && Arrays.stream(player.getAgents()).anyMatch(agent -> agent.equalsIgnoreCase(agentName)))
+                .filter(player -> player.getOrg().equalsIgnoreCase(org) && Arrays.stream(player.getAgents().split(", ")).anyMatch(agent -> agent.equalsIgnoreCase(agentName)))
                 .collect(Collectors.toList());
     }
 
