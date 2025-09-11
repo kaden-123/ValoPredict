3e2908ad74fa3fd3c7ccb6c496f2c8aa42eeec41
David An <totallydavidahn@gmail.com>
2025-09-10 18:40:26 -0700

    Create PlayerFilterRequest class to represent query string parameters for filtering players

---
+++ backend/src/main/java/com/valorant/ValoPredict/player/PlayerFilterRequest.java
@@ -0,0 +1,271 @@
+package com.valorant.ValoPredict.player;
+
+public class PlayerFilterRequest {
+
+    private String roundsOp;
+
+    private Integer rounds_played;
+
+    private String ratingOp;
+
+    private Double rating;
+
+    private String acsOp;
+
+    private Double acs;
+
+    private String kdOp;
+
+    private Double kd;
+
+    private String kastOp;
+
+    private Integer kast;
+
+    private String adrOp;
+
+    private Double adr;
+
+    private String kprOp;
+
+    private Double kpr;
+
+    private String aprOp;
+
+    private Double apr;
+
+    private String fkprOp;
+
+    private Double fkpr;
+
+    private String fdprOp;
+
+    private Double fdpr;
+
+    private String hs_percentageOp;
+
+    private Integer hs_percentage;
+
+    private String clutch_success_percentageOp;
+
+    private Integer clutch_success_percentage;
+
+    public PlayerFilterRequest(String roundsOp, Integer rounds_played, String ratingOp, Double rating, String acsOp, Double acs, String kdOp, Double kd, String kastOp, Integer kast, String adrOp, Double adr, String kprOp, Double kpr, String aprOp, Double apr, String fkprOp, Double fkpr, String fdprOp, Double fdpr, String hs_percentageOp, Integer hs_percentage, String clutch_success_percentageOp, Integer clutch_success_percentage) {
+        this.roundsOp = roundsOp;
+        this.rounds_played = rounds_played;
+        this.ratingOp = ratingOp;
+        this.rating = rating;
+        this.acsOp = acsOp;
+        this.acs = acs;
+        this.kdOp = kdOp;
+        this.kd = kd;
+        this.kastOp = kastOp;
+        this.kast = kast;
+        this.adrOp = adrOp;
+        this.adr = adr;
+        this.kprOp = kprOp;
+        this.kpr = kpr;
+        this.aprOp = aprOp;
+        this.apr = apr;
+        this.fkprOp = fkprOp;
+        this.fkpr = fkpr;
+        this.fdprOp = fdprOp;
+        this.fdpr = fdpr;
+        this.hs_percentageOp = hs_percentageOp;
+        this.hs_percentage = hs_percentage;
+        this.clutch_success_percentageOp = clutch_success_percentageOp;
+        this.clutch_success_percentage = clutch_success_percentage;
+    }
+
+    public String getRoundsOp() {
+        return roundsOp;
+    }
+
+    public void setRoundsOp(String roundsOp) {
+        this.roundsOp = roundsOp;
+    }
+
+    public Integer getRounds_played() {
+        return rounds_played;
+    }
+
+    public void setRounds_played(Integer rounds_played) {
+        this.rounds_played = rounds_played;
+    }
+
+    public String getRatingOp() {
+        return ratingOp;
+    }
+
+    public void setRatingOp(String ratingOp) {
+        this.ratingOp = ratingOp;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getAcsOp() {
+        return acsOp;
+    }
+
+    public void setAcsOp(String acsOp) {
+        this.acsOp = acsOp;
+    }
+
+    public Double getAcs() {
+        return acs;
+    }
+
+    public void setAcs(Double acs) {
+        this.acs = acs;
+    }
+
+    public String getKdOp() {
+        return kdOp;
+    }
+
+    public void setKdOp(String kdOp) {
+        this.kdOp = kdOp;
+    }
+
+    public Double getKd() {
+        return kd;
+    }
+
+    public void setKd(Double kd) {
+        this.kd = kd;
+    }
+
+    public String getKastOp() {
+        return kastOp;
+    }
+
+    public void setKastOp(String kastOp) {
+        this.kastOp = kastOp;
+    }
+
+    public Integer getKast() {
+        return kast;
+    }
+
+    public void setKast(Integer kast) {
+        this.kast = kast;
+    }
+
+    public String getAdrOp() {
+        return adrOp;
+    }
+
+    public void setAdrOp(String adrOp) {
+        this.adrOp = adrOp;
+    }
+
+    public Double getAdr() {
+        return adr;
+    }
+
+    public void setAdr(Double adr) {
+        this.adr = adr;
+    }
+
+    public String getKprOp() {
+        return kprOp;
+    }
+
+    public void setKprOp(String kprOp) {
+        this.kprOp = kprOp;
+    }
+
+    public Double getKpr() {
+        return kpr;
+    }
+
+    public void setKpr(Double kpr) {
+        this.kpr = kpr;
+    }
+
+    public String getAprOp() {
+        return aprOp;
+    }
+
+    public void setAprOp(String aprOp) {
+        this.aprOp = aprOp;
+    }
+
+    public Double getApr() {
+        return apr;
+    }
+
+    public void setApr(Double apr) {
+        this.apr = apr;
+    }
+
+    public String getFkprOp() {
+        return fkprOp;
+    }
+
+    public void setFkprOp(String fkprOp) {
+        this.fkprOp = fkprOp;
+    }
+
+    public Double getFkpr() {
+        return fkpr;
+    }
+
+    public void setFkpr(Double fkpr) {
+        this.fkpr = fkpr;
+    }
+
+    public String getFdprOp() {
+        return fdprOp;
+    }
+
+    public void setFdprOp(String fdprOp) {
+        this.fdprOp = fdprOp;
+    }
+
+    public Double getFdpr() {
+        return fdpr;
+    }
+
+    public void setFdpr(Double fdpr) {
+        this.fdpr = fdpr;
+    }
+
+    public String getHs_percentageOp() {
+        return hs_percentageOp;
+    }
+
+    public void setHs_percentageOp(String hs_percentageOp) {
+        this.hs_percentageOp = hs_percentageOp;
+    }
+
+    public Integer getHs_percentage() {
+        return hs_percentage;
+    }
+
+    public void setHs_percentage(Integer hs_percentage) {
+        this.hs_percentage = hs_percentage;
+    }
+
+    public String getClutch_success_percentageOp() {
+        return clutch_success_percentageOp;
+    }
+
+    public void setClutch_success_percentageOp(String clutch_success_percentageOp) {
+        this.clutch_success_percentageOp = clutch_success_percentageOp;
+    }
+
+    public Integer getClutch_success_percentage() {
+        return clutch_success_percentage;
+    }
+
+    public void setClutch_success_percentage(Integer clutch_success_percentage) {
+        this.clutch_success_percentage = clutch_success_percentage;
+    }
+}
