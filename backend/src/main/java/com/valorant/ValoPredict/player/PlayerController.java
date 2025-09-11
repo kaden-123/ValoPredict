53e9554e948426f7a20d49435855f50d5218c2b0
David An <totallydavidahn@gmail.com>
2025-09-10 18:41:10 -0700

    Add mapping for filtering players

--- backend/src/main/java/com/valorant/ValoPredict/player/PlayerController.java
+++ backend/src/main/java/com/valorant/ValoPredict/player/PlayerController.java
@@ -44,6 +44,79 @@ public class PlayerController {
         }
     }
 
+    @GetMapping("/sort")
+    public List<Player> getSortingMethod(
+            @RequestParam(required = false) boolean rounds_played,
+            @RequestParam(required = false) boolean rating,
+            @RequestParam(required = false) boolean acs,
+            @RequestParam(required = false) boolean kd,
+            @RequestParam(required = false) boolean kast,
+            @RequestParam(required = false) boolean adr,
+            @RequestParam(required = false) boolean kpr,
+            @RequestParam(required = false) boolean apr,
+            @RequestParam(required = false) boolean fkpr,
+            @RequestParam(required = false) boolean fdpr,
+            @RequestParam(required = false) boolean hs_percentage,
+            @RequestParam(required = false) boolean clutch_success_percentage,
+            @RequestParam(required = false) boolean reversed){
+        if(rounds_played){
+            return playerService.sortPlayersByRoundsPlayed(reversed);
+        }
+
+        else if(rating){
+            return playerService.sortPlayersByRating(reversed);
+        }
+
+        else if(acs){
+            return playerService.sortPlayersByAcs(reversed);
+        }
+
+        else if(kd){
+            return playerService.sortPlayersByKd(reversed);
+        }
+
+        else if(kast){
+            return playerService.sortPlayersByKast(reversed);
+        }
+
+        else if(adr){
+            return playerService.sortPlayersByAdr(reversed);
+        }
+
+        else if(kpr){
+            return playerService.sortPlayersByKpr(reversed);
+        }
+
+        else if(apr){
+            return playerService.sortPlayersByApr(reversed);
+        }
+
+        else if(fkpr){
+            return playerService.sortPlayersByFkpr(reversed);
+        }
+
+        else if(fdpr){
+            return playerService.sortPlayersByFdpr(reversed);
+        }
+
+        else if(hs_percentage){
+            return playerService.sortPlayersByHsPercentage(reversed);
+        }
+
+        else if(clutch_success_percentage){
+            return playerService.sortPlayersByClutchSuccessPercentage(reversed);
+        }
+
+        else{
+            return playerService.getPlayers();
+        }
+    }
+
+    @GetMapping("/filter")
+    public List<Player> getFilteredPlayers(@ModelAttribute PlayerFilterRequest filter){
+        return playerService.filterPlayers(filter);
+    }
+
     @PostMapping
     public ResponseEntity<Player> addPlayer(@RequestBody Player player){
         Player newPlayer = playerService.addPlayer(player);
