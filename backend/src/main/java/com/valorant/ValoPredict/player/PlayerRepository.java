73f2615c0d6ada591764953af7a30a49c44d0838
David An <totallydavidahn@gmail.com>
2025-09-10 18:43:09 -0700

    Extend JpaSpecificationExecutor to create Specification<T> objects

--- backend/src/main/java/com/valorant/ValoPredict/player/PlayerRepository.java
+++ backend/src/main/java/com/valorant/ValoPredict/player/PlayerRepository.java
@@ -1,10 +1,11 @@
 package com.valorant.ValoPredict.player;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
+public interface PlayerRepository extends JpaRepository<Player, Integer>, JpaSpecificationExecutor<Player> {
     // void deleteById(Integer id);
     // Optional<Player> findById(Integer id);
 }
